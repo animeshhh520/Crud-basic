e65845c560a781c6927a916d4a352ff2064a0ff1
animeshhh520 <131814632+animeshhh520@users.noreply.github.com>
2025-08-11 21:12:23 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Your MySQL username
-$password = ""; // Your MySQL password
+$username = "root";
+$password = ""; 
 $dbname = "student_management";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
